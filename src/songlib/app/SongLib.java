e30382d0c399dd62370f806fd2c43b64573e7df1
llongc <cynthia.d.long@outlook.com>
2019-09-25 21:34:15 -0400

    mainstage setup

--- src/songlib/app/SongLib.java
+++ src/songlib/app/SongLib.java
@@ -5,21 +5,29 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.AnchorPane;
+//import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
+import songlib.view.SongLibController;;
 
 public class SongLib extends Application{
 	@Override
 	public void start(Stage primaryStage) 
 	throws IOException {
-		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(getClass().getResource("/songlib/view/songlib.fxml"));
-		GridPane root = (GridPane)loader.load();
-		Scene scene = new Scene(root);
+		
+		FXMLLoader loader = new FXMLLoader();   
+		loader.setLocation(
+				getClass().getResource("/songlib/view/songlib.fxml"));
+		AnchorPane root = (AnchorPane)loader.load();
+
+
+		SongLibController listController = loader.getController();
+		listController.start(primaryStage);
+
+		Scene scene = new Scene(root, 200, 300);
 		primaryStage.setScene(scene);
-		primaryStage.setTitle("Fahrenheit-Celsius");
-		primaryStage.setResizable(false);  
-		primaryStage.show();
+		primaryStage.show(); 
+		
 	}
 	
 	public static void main(String[] args) {
--- src/songlib/view/SongLibController.java
+++ src/songlib/view/SongLibController.java
@@ -1,10 +1,44 @@
 package songlib.view;
 
-import javafx.event.ActionEvent;
+//import java.util.Optional;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+//import javafx.scene.control.Alert;
+//import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ListView;
+//import javafx.scene.control.TextInputDialog;
+import javafx.stage.Stage;
 
 public class SongLibController {
-
+	@FXML
+	ListView<String> listView;
+	
+	private ObservableList<String> obsList;
+	
+	public void start(Stage mainStage) {
+		// create an ObservableList 
+		// from an ArrayList  
+		obsList = FXCollections.observableArrayList(                               
+				"Giants",                               
+				"Patriots",
+				"49ers",
+				"Rams",
+				"Packers",
+				"Colts",
+				"Cowboys",
+				"Broncos",
+				"Vikings",
+				"Dolphins",
+				"Titans",
+				"Seahawks",
+				"Steelers",
+				"Jaguars"); 
+		
+		listView.setItems(obsList); 
+		
+		// select the first item
+	    listView.getSelectionModel().select(0);
+	}
 }
